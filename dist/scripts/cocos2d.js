c5dadcbea21fd350cd7f2212dcafccfac5c10617
Christian Schwartz <christian.schwartz@gmail.com>
2013-08-24 21:44:40 +0200

    switched to single engine file

--- dist/scripts/cocos2d.js
+++ dist/scripts/cocos2d.js
@@ -35,8 +35,8 @@
         loadExtension:false,
         renderMode:0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
         tag:'gameCanvas', //the dom element to run cocos2d on
-        engineDir:'scripts/vendor/cocos2d/',
-        // SingleEngineFile:'scripts/vendor/cocos2d-html5.min.js',
+        //engineDir:'scripts/vendor/cocos2d/',
+         SingleEngineFile:'scripts/vendor/cocos2d-html5.min.js',
         appFiles:[
             'scripts/resource.js',
             'scripts/player.js',
